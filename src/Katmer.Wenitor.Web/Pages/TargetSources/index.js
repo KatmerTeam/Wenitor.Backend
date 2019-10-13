be2f090b4f40b59f447db08dfa0c9167d5aca1ca
Mehmet Yüksel POLAT <mypolat@outlook.com>
2019-10-13 23:04:22 +0300

    Fixed "TargetSource" Index.js Path

--- src/Katmer.Wenitor.Web/Pages/TargetSources/index.js
+++ src/Katmer.Wenitor.Web/Pages/TargetSources/index.js
@@ -1,6 +1,6 @@
 ﻿$(function () {
     var dataTable = $('#TargetSourcesTable').DataTable(abp.libs.datatables.normalizeConfiguration({
-        ajax: abp.libs.datatables.createAjax(katmer.wenitor.targetSources.getList),
+        ajax: abp.libs.datatables.createAjax(katmer.wenitor.targetSource.getList),
         columnDefs: [
             { data: "name" },
         ]
